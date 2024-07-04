5c87525da905950ea412374c35859c92ca5a35b1
Jonathan Curtis <jon.curtis.94@gmail.com>
2024-07-04 14:33:41 +0200

    Updated python implementation to be more efficient. Working on adding FFTW to C++ to bypass Python.

--- XYVortices.py
+++ XYVortices.py
@@ -10,37 +10,51 @@ import time
 data_directory = "../../data/"
 
 
-
-def genThetas(L,T,ntimes,J=1.,dt = .05):
+### Function which will accept simulation parameters and return a thermalized initial state
+def gen_burned_thetas(L,T,nburn,J=1.,dt = .05):
 	"""Generates a sequence of angles for a given system size, temperature, number of time steps, Josephson coupling (default is 1.) and time step size (default is 5% J)"""
 	### Uses periodic boundary conditions
 	### defaul time step is 5% of J
 
-	thetas = np.zeros((ntimes,L,L))
+	thetas = np.zeros((L,L))
+	tmp = np.zeros_like(thetas)
 
-	for nt in range(1,ntimes):
+	for nt in range(1,nburn):
 		for k in range(L*L):
 			x = k//L
 			y = k % L
-			thetas[nt,x,y] = thetas[nt-1,x,y] - J*dt*(
-				np.sin( thetas[nt-1,x,y] - thetas[nt-1,(x+1)//L,y]) 
-				+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x-1,y]) 
-				+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x,(y+1)//L]) 
-				+np.sin( thetas[nt-1,x,y] - thetas[nt-1,x,y-1])
+			tmp[x,y] =  - J*dt*(
+				np.sin( thetas[x,y] - thetas[(x+1)//L,y]) 
+				+np.sin( thetas[x,y] - thetas[x-1,y]) 
+				+np.sin( thetas[x,y] - thetas[x,(y+1)//L]) 
+				+np.sin( thetas[x,y] - thetas[x,y-1])
 				)
+		thetas += tmp
 
-		thetas[nt,:,:] +=  np.random.normal(0.,2.*T*dt,size=(L,L))
+		thetas +=  np.random.normal(0.,2.*T*dt,size=(L,L))
 
 	return thetas
 
-def calcOP(thetas):
+### Given a time-slice value of thetas[x,y] this returns the spatial average of the order parameter 
+def calc_OP(thetas):
 	"""Calculates the spatial average of order parameter over space"""
-	s = thetas.shape
-	ntimes = s[0]
-	OP = np.zeros(ntimes,dtype=complex)
 	OP = np.mean(np.exp(1.j*thetas),axis=(-1,-2))
 
 	return OP
+
+### Given a time-slice of values of thetas[x,y] this returns the vorticity density 
+def calc_vort(thetas):
+	vorticity = np.zeros_like(thetas)
+
+	for k in range(L*L):
+		x = k//L
+		y = k % L
+		vorticity[x,y] = ( np.mod(np.thetas[(x+1)//L,y] - thetas[x,y],2.*np.pi) 
+			+np.mod( thetas[(x+1)//L,(y+1)//L] - thetas[(x+1)//L,y],2.*np.pi) 
+			+np.mod( thetas[x,(y+1)//L] - thetas[(x+1)//L,(y+1)//L],2.*np.pi) 
+			+np.mod( thetas[x,y] - thetas[x,(y+1)//L],2.*np.pi) )
+
+	return vorticity
 
 ### Computes instantaneous magnetic field noise at NV location given an instantenous profile for the theta field
 def calc_NV_noise(z,thetas):
@@ -72,36 +86,38 @@ def calc_NV_noise(z,thetas):
 
 
 
-
-
-
-
 def main():
 
 	save_data = False
 	data_directory = "../../data/"
 
 
-	L = 50### Lattice size -- LxL lattice
+	L = 40 ### Lattice size -- LxL lattice
 
-	num_temps = 20
-	temps = np.linspace(0.1,5.,num_temps)
+	ntemps = 4
+	temps = np.linspace(0.1,4.,ntemps)
 
-	nburn = 5000### Time steps we burn initially to equilibrate
-	ntimes = 4000### Number of times steps we calculate and measure for
+	nburn = 10000### Time steps we burn initially to equilibrate
 
 	ti = time.time()
 
-	ops = np.zeros(num_temps,dtype=complex)
+	ops = np.zeros(ntemps,dtype=complex)
+	vorticity = np.zeros((ntemps,L,L))
 
-	for i in range(num_temps):
-		thetas = genThetas(L,temps[i],nburn+ntimes)
+	for i in range(ntemps):
+		thetas = gen_burned_thetas(L,temps[i],nburn)
 
-		ops[i] = np.mean(calcOP(thetas)[nburn:])
+		ops[i] = calc_OP(thetas)
+		vorticity[i,:,:] = calc_vort(thetas)
 
 
 	tf = time.time()
 	print("Elapsed total time: ",tf-ti,"s")
+
+	for i in range(ntemps):
+		plt.imshow(vorticity[i,:,:])
+		plt.colorbar()
+		plt.show()
 
 	if save_data:
 		np.save(data_directory+"thetas.npy",thetas)
