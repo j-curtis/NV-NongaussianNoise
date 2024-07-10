24eabb56b29f6d0628b56941c2ee090fd95b758d
Jonathan Curtis <jon.curtis.94@gmail.com>
2024-07-10 13:30:20 +0200

    Writing method to convert vorticity into distribution of noise at qubit distance

--- XYVortices.py
+++ XYVortices.py
@@ -6,8 +6,9 @@ import numpy as np
 from matplotlib import pyplot as plt 
 import time 
 
-
-data_directory = "../../data/"
+#######################################
+### THESE METHODS ARE FOR RUNNING SIMULATION IN PYTHON
+#######################################
 
 ### Function which will accept simulation parameters and return a thermalized initial state
 def gen_burned_thetas(L,T,nburn,J=1.,dt = .05):
@@ -56,8 +57,9 @@ def calc_vort(thetas):
 	return vorticity
 
 
-
+#######################################
 ### HERE IS A SET OF FUNCTIONS FOR PROCESSING VORTICITY PROFILES FROM C++ SIMULATIONS
+#######################################
 
 ### This method returns a numpy data file obtained from reading a csv file of the format of LxL doubles per line with each line a time step 
 def load_csv(fname):
@@ -72,18 +74,58 @@ def load_csv(fname):
 	return data
 
 
-### This method will perform an fft on the vorticity data 
+### This method will take the spatial vorticity profile and compute the filtered FFT at a depth z (accepts an array of distances z)
 ### vorticity is assumed to be a single time slice and is LxL
-def fft_vorticity(vorticity):
-	
-	fft_out = np.fft.fft2(data)
-	return fft_out
+def fft_filtered(vorticity,z_list):
 
-	
+	### First we extract how many z points we will be computing for 
+	num_zs = len(z_list)
+
+	### We will need FFT only once for all z points
+	### We compute here 
+	### We compute the real fft but insist it have the same output shape as the input array, for convenience
+	fft = np.fft.rfft2(data,s=vorticity.shape)
+
+	### We also need the corresponding momentum space points to compute the filters 
+	### This also is needed only once for all z points
+	qs = np.linspace(0.,2.*np.pi,L)
+
+	### Now we compute the filter functions, which has one for each z point we want 
+	filter_funcs = np.zeros((num_zs,L,L))
+
+	### Output array for filtered FFT at each distance z in the list 
+	out = np.zeros((num_zs,L,L),dtype=complex)
+
+	for nz in range(num_zs):
+		z = z_list[nz]
+
+		for nx in range(L):
+			for ny in range(L):
+				q = np.sqrt(qs[nx]**2 + qs[ny]**2)
+			
+				filter_funcs[nz,nx,ny] = np.exp(-2.*z*q)/(2.*q + .00001)**2 ### For q -> 0 we shift slightly, it should be ultimately suppressed anyways
+
+		out[nz,:,:] = fft*filter_funcs[nz,:,:]
+
+
+	return out
+
+
 def main():
 
+	z_list = np.array([1.,3.,10.,30.,100.,300.,1000.,3000.])
 
+	data_directory = "../data/"
+	files = ["vorticity_L=30_t=5000_T=1.500000"]
 
+	paths = [data_directory + file for file in files]
+
+	data_files = [ load_csv(path) for path in paths]
+
+	for data in data_files:
+
+		for nt in range(len(data.shape[0])):
+			bfield_z = fft_filtered(data[nt,:,:],z_list)
 
 if __name__ == "__main__":
 	main()
