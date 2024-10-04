d5ec08a9a5a66f63ebd15944fc77c86379326c6b
Jonathan Curtis <jon.curtis.94@gmail.com>
2024-10-04 15:34:45 +0200

    Implementing method to compute the non-Gaussian diagnostic for higher spins

--- telegraphHigherSpin.py
+++ telegraphHigherSpin.py
@@ -61,7 +61,7 @@ def kern_step(S,dt):
 #####################################
 ### Two-time correlation function ###
 #####################################
-
+"""
 ### This function will find the spin-spin correlation function 
 ### For the times we pass dt and a number of steps nt and return values for all integers up to nt 
 def calc_g2(S,dt,nt):
@@ -84,16 +84,120 @@ def calc_g2(S,dt,nt):
 	return g2s
 
 
-def main():
+"""
+
+
+#####################################
+### Two-time correlation function ###
+#####################################
+### This function will find the spin-spin correlation function 
+### For the times we pass two-times and return g2(t2,t1) 
+def calc_g2(S,t1,t2):
+	M = int( 2*S + 1)
+
+	S_vec = np.arange(M) - S*np.ones(M) ### This should be a vector with components [-S,-S+1,...,S-1,S]
+
+	matrix = rate_matrix(S)
+
+	kern = linalg.expm((t2-t1)*matrix)
+
+	return S_vec@kern@S_vec/float(M) ### This is the two-time spin correlation function
+
+
+######################################
+### Four-time correlation function ###
+######################################
+
+### This function will find the spin-spin four-time correlation function 
+### For the times we pass four times return the function g(t4,t3,t2,t1) 
+### The time steps are assumed to be ordered such that t4,t3 > t2,t1
+def calc_g4(S,t1,t2,t3,t4):
+	M = int( 2*S + 1)
+
+	S_vec = np.arange(M) - S*np.ones(M) ### This should be a vector with components [-S,-S+1,...,S-1,S]
+	S_mat = np.diag(S_vec)
+
+
+	rates = rate_matrix(S)
+
+	### Kernels for the three different time-delays between the four times 
+
+	kern1 = linalg.expm(np.abs(t2-t1)*rates)
+	kern2 = linalg.expm( (min(t4,t3) - max(t2,t1))*rates )
+	kern3 = linalg.expm(np.abs(t4-t3)*rates)
+
 	
-	S = 1.5
+	return S_vec@kern3@kern2@S_mat@kern1@S_vec/float(M) ### This is the four-time spin correlation function
+
+
+####################################
+### Four-time connected cumulant ###
+####################################
+
+### This function will find the spin-spin four-time correlation function 
+### For the times we pass four times return the function g(t4,t3,t2,t1) 
+### The time steps are assumed to be ordered such that t4,t3 > t2,t1
+### This is g4(t1,t2,t3,t4) - g2(t1,t2)g2(t3,t4) - g2(t1,t3)g2(t2,t4) - g2(t1,t4)g2(t2,t3)
+def calc_g4conn(S,t1,t2,t3,t4):
+	g4 = calc_g4(S,t1,t2,t3,t4)
+
+	g2_12 = calc_g2(S,t1,t2)
+	g2_13 = calc_g2(S,t1,t3)
+	g2_14 = calc_g2(S,t1,t4)
+
+	g2_23 = calc_g2(S,t2,t3)
+	g2_24 = calc_g2(S,t2,t4)
+
+	g2_34 = calc_g2(S,t3,t4)
+
+	return - ( g4 - (g2_12*g2_34 + g2_13*g2_24 + g2_14*g2_23) )
+
+####################################
+### Fourth order noise Xcumulant ###
+####################################
+### This function will find the relevant integral for the integral of the connected cumulant over the echo times
+### We evaluate using a monte carlo method for the times 
+### This relies on int_{-T}^0 dt f(t) = T x sum_j f(t_j) where t_j are randomly chosen from [-T,0] and same for [0,T] integral 
+### This then gives T^4 x sum_{jklm} ... for the four randomly sampled times 
+
+def calc_Xcumulant(S,T,npts):
+	### T is the echo time and S the spin. npts is the number of monte carlo samples 
+	tpts = T*np.random.ranf((npts,4)) ### Random time points all in the interval of [0,T]
+	### We how shift the first two to be in the range [-T,0]
+	tpts[:,:2] += -np.ones((npts,2))*T 
+
+	### Now we evaluate the cummulant function for each of these time points and tally up the average 
+	connected_correlations = np.zeros(npts)
+
+	for i in range(npts):
+		connected_correlations[i] = calc_g4conn(S,tpts[i,0],tpts[i,1],tpts[i,2],tpts[i,3])
+
+	return T**4 * np.mean(connected_correlations)
+
+
+
+
+def main():
+
+	S = 3.
+	T = 1.
+	npts = 5000
+	xcum = calc_Xcumulant(S,T,npts)
+	print(xcum)
+	quit()
+	
+	S = [ .5,1.,1.5,2.,2.5,3.]
 	dt = 0.05
-	nt = 30
+	nt = 100
 	times = np.linspace(0.,dt*(nt-1),nt)
 
-	g2 = calc_g2(S,dt,nt)
+	g2s = [ calc_g2(s,dt,nt) for s in S ]
 
-	plt.plot(g2)
+	for s in range(len(S)):
+		plt.plot(times,g2s[s])
+
+	plt.plot(times,np.exp(-2.*times),marker='x')
+	plt.yscale('log')
 	plt.show()
 
 
